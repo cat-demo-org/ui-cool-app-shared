916f61f25f0ac5a770b6eef3d123fb626908ea2b
ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>
2023-09-07 13:20:42 -0400

    init

--- dist/redux/hooks.d.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook } from 'react-redux';
-import { type RootState, type AppDispatch, type ApplicationState } from './store';
-import { DemoSlice } from './reducers/demoSlice';
-export declare const useAppDispatch: () => AppDispatch;
-export declare const useAppSelector: TypedUseSelectorHook<RootState>;
-export declare const extractDemoSlice: (state: ApplicationState) => DemoSlice;
